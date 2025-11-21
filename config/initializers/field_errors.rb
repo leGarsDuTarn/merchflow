d40f81f1cd3f95507a7b8a610bfc859bd199c26d
Benjamin Grassiano <grassiano.b@gmail.com>
2025-11-21 09:31:52 +0100

    Debugging champs de formulaire incorrect + modif methode pour logout

--- config/initializers/field_errors.rb
+++ config/initializers/field_errors.rb
@@ -1,17 +1,29 @@
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  errors = Array(instance.error_message).join(', ')
+# Configuration du comportement des erreurs de formulaire dans Rails
+# Cette procédure est appelée automatiquement par Rails pour chaque champ
+# qui contient une erreur de validation
 
-  # Ajoute une classe CSS "is-invalid-orange"
-  if html_tag =~ /^<input|^<textarea|^<select/
-    new_tag = html_tag.sub(/class="/, 'class="is-invalid-orange ')
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+
+  # On vérifie si la balise HTML est un champ de formulaire (input, textarea, select)
+  # et non un label. Cela évite de styliser les labels avec la classe d'erreur.
+  if html_tag =~ /<(input|textarea|select)/
+
+    # On assigne directement le résultat du conditionnel à la variable new_tag
+    # Cas 1 : La balise possède déjà un attribut "class"
+    # On ajoute simplement notre classe "is-invalid-orange" aux classes existantes
+    # Cas 2 : La balise n'a pas encore d'attribut "class"
+    # On ajoute un nouvel attribut class avec notre classe d'erreur
+    new_tag = if html_tag =~ /class="/
+                html_tag.sub('class="', 'class="is-invalid-orange ')
+              else
+                html_tag.sub(/\/>|>/, ' class="is-invalid-orange"\0')
+              end
+
+    # On retourne la nouvelle balise modifiée comme HTML sécurisé
+    new_tag.html_safe
+
   else
-    new_tag = html_tag
+    # Si c'est un label ou un autre élément, on le retourne tel quel sans modification
+    html_tag.html_safe
   end
-
-  <<-HTML.html_safe
-    <div class="field-with-errors">
-      #{new_tag}
-      <p class="error-text mt-1 mb-0">#{errors}</p>
-    </div>
-  HTML
 end
