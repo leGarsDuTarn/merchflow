78a151a152f9f248c11a58f66b85824af6044c8e
Benjamin Grassiano <grassiano.b@gmail.com>
2025-12-31 09:28:17 +0100

    Modifi initializers mailjet

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,6 +1,7 @@
 # config/initializers/mailjet.rb
 Mailjet.configure do |config|
-  config.api_key = ENV['MAILJET_API_KEY']
+  config.api_key = ENV['MAILJET_API_KEY']       
   config.secret_key = ENV['MAILJET_SECRET_KEY']
   config.default_from = 'contact@merchflow.fr'
+  config.api_version = 'v3.1'
 end
