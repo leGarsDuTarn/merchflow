0d61029cf24b979547211dbbf3445facdfd3042d
Benjamin Grassiano <grassiano.b@gmail.com>
2026-01-12 16:42:04 +0100

    Correction des test v1

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -18,20 +18,16 @@ RSpec.describe JobApplication, type: :model do
     it 'copie les infos de l\'offre à la création (Snapshot)' do
       application = create(:job_application, job_offer: offer, merch: merch)
 
-      # On vérifie que les champs snapshot sont remplis
-      expect(application.job_title_snapshot).to eq("Super Mission")
+      expect(application.job_title_snapshot).to eq("Super mission")
       expect(application.hourly_rate_snapshot).to eq(14.50)
       expect(application.location_snapshot).to include("Bordeaux")
     end
 
     it 'garde les infos snapshot même si l\'offre change après' do
       application = create(:job_application, job_offer: offer, merch: merch)
-
-      # L'agence modifie l'offre
       offer.update!(title: "Titre Modifié", hourly_rate: 12.02)
 
-      # La candidature doit garder les anciennes valeurs (Preuve juridique)
-      expect(application.job_title_snapshot).to eq("Super Mission")
+      expect(application.job_title_snapshot).to eq("Super mission")
       expect(application.hourly_rate_snapshot).to eq(14.50)
     end
   end
